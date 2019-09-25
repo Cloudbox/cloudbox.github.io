dc87a5195d32f827b7115b74fc2091f3dc628920
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-09-25 15:46:57 -0500

    Squashed 'scripts/' content from commit d372933
    git-subtree-dir: scripts
    git-subtree-split: d372933d8076331643070022937df10b4af826c7

---
+++ dep_ipv6.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+#################################################################################
+# Title:         Cloudbox: Dependencies Installer (IPv6)                        #
+# Author(s):     L3uddz, Desimaniac, EnorMOZ                                    #
+# URL:           https://github.com/Cloudbox/Cloudbox                           #
+# Description:   Installs dependencies needed for Cloudbox.                     #
+# --                                                                            #
+#             Part of the Cloudbox project: https://cloudbox.works              #
+#################################################################################
+#                     GNU General Public License v3.0                           #
+#################################################################################
+# Usage:                                                                        #
+# ======                                                                        #
+# curl -s https://cloudbox.works/scripts/dep_ipv6.sh | sudo sh                  #
+# wget -qO- https://cloudbox.works/scripts/dep_ipv6.sh | sudo sh                #
+#                                                                               #
+# Custom Ansible Version:                                                       #
+# curl -s https://cloudbox.works/scripts/dep_ipv6.sh | sudo sh -s <version>     #
+# wget -qO- https://cloudbox.works/scripts/dep_ipv6.sh | sudo sh -s <version>   #
+#################################################################################
+
+## Constants
+readonly PIP="9.0.3"
+readonly ANSIBLE="2.5.14"
+
+## AppVeyor
+if [ "$SUDO_USER" = "appveyor" ]; then
+    rm /etc/apt/sources.list.d/*
+    rm /etc/apt/sources.list
+    curl https://cloudbox.works/scripts/apt-sources/xenial.txt | tee /etc/apt/sources.list
+    apt-get update
+fi
+
+## Environmental Variables
+export DEBIAN_FRONTEND=noninteractive
+
+## Remove IPv6 entries from systctl
+sed -i -e '/^net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+sed -i -e '/^net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+sed -i -e '/^net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
+sysctl -p
+
+## Install Pre-Dependencies
+apt-get install -y --reinstall \
+    software-properties-common
+
+## Add apt repos
+add-apt-repository main
+add-apt-repository universe
+add-apt-repository restricted
+add-apt-repository multiverse
+apt-get update
+
+## Install apt Dependencies
+apt-get install -y --reinstall \
+    nano \
+    git \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python3-dev \
+    python3-pip \
+    python-dev \
+    python-pip
+
+## Install pip3 Dependencies
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pip==${PIP}
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    setuptools
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pyOpenSSL \
+    requests \
+    netaddr
+
+## Install pip2 Dependencies
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pip==${PIP}
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    setuptools
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+    pyOpenSSL \
+    requests \
+    netaddr \
+    jmespath \
+    ansible==${1-$ANSIBLE}
+
+## Copy pip to /usr/bin
+cp /usr/local/bin/pip /usr/bin/pip
+cp /usr/local/bin/pip3 /usr/bin/pip3
